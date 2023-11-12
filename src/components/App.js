f515709439e9951bab092a3d72da0717d4c00f13
Musab Sakhreyah <mosabkhaled1998@gmail.com>
2023-11-12 09:29:45 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,16 @@
 // create your App component here
+import { useEffect, useState } from "react"
+function App() {
+    const [randImage, setRandImage] = useState(null)
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(r => r.json())
+            .then((image) => setRandImage(image.message))
+    }, [])
+
+    return (
+        !randImage ? <p>Loading...</p> : <img src={randImage} alt={"A Random Dog"}></img>
+    )
+}
+
+export default App;
